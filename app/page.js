efa56df7aacbfafe9beb7a0b82a03e6433e7a980
MDBBee <bobbyugbebor@gmail.com>
2025-01-20 04:26:33 +0200

    Drinks-activity_01 ✅

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-7xl">HomePage</h1>
+      <h1 className="text-5xl mb-8 font-bold">Drinks Mini</h1>
+      <Link href="/client" className="btn btn-accent">
+        get started
+      </Link>
     </div>
   );
 };
